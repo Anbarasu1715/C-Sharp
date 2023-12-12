1801115cdc8729affe2be80f94c46a34c7599475
Anbarasu <anbarasu47442003@gmail.com>
2023-12-12 11:33:55 +0530

    By Anbarasu

---
+++ Delegates/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class ProductService
+    {
+
+        public delegate void deliverycheckEventHandler(Object obj,EventArgs args);
+        public event deliverycheckEventHandler deliverycheck;
+
+        public void Delivery(Factory factoryProuct) {
+            Console.WriteLine("Product ready for delivery.....");
+            Thread.Sleep(3000);
+
+            onDeliveryCheck();
+        }
+
+        protected virtual void onDeliveryCheck() {
+            if (deliverycheck!=null) {
+                deliverycheck(this,EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factoryProduct = new Factory() { prodId=1,prodName="Steels"};
+            ProductService prod = new ProductService();
+            deliveryService ds = new deliveryService();
+            NewdeliveryService nds = new NewdeliveryService();
+
+
+            prod.deliverycheck += nds.onDeliveryCheck;
+            prod.deliverycheck += ds.onDeliveryCheck;
+            prod.Delivery(factoryProduct);
+            Console.WriteLine("Press any key to exit.....!");
+            Console.ReadKey();
+        }
+
+
+    }
+
+    public class deliveryService {
+        public void onDeliveryCheck(object src, EventArgs args) {
+            Console.WriteLine("Product delivered successfully.....!");
+            Console.WriteLine(args.GetType());
+        }
+    }
+
+    public class NewdeliveryService
+    {
+        public void onDeliveryCheck(object src, EventArgs args)
+        {
+            Console.WriteLine("Product delivered successfully by New Delivery System.....!");
+            Console.WriteLine(args.GetType());
+        }
+    }
+
+}
